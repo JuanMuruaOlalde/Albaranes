ef0eacedd8832a434356bc156e2c02c9c19496c0
Juan Murua Olalde <juan@susosise.es>
2023-01-30 17:28:28 +0100

    bosquejar el javascript
    Por ahora muesta las cabeceras de los albaranes.
    
    Falta la parte de mostar el detalle de lineas.

--- src/main/resources/static/js/ConsultaConPaginaDinamica.js
+++ src/main/resources/static/js/ConsultaConPaginaDinamica.js
@@ -8,12 +8,31 @@ window.addEventListener("load", () => {
     function obtenerAlbaranes() {
         const datos = new FormData(frmObtenerAlbaranes);
         const cuantos = Number(datos.get("cuantosAlbaranes"));
+        fetch("/albaranes/ultimos/" + cuantos)
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No ha sido posible obtener datos de albaranes:\n" + response.status + " " + response.statusText);
+                }
+                return response.json();
+            })
+            .then((jsonData) => {
+                const tablaDeAlbaranes = document.getElementById("tablaDeLosAlbaranes");
+                tablaDeAlbaranes.innerHTML = 
+                    "<thead><th>numero</th><th>cliente</th><th>para entregar en</th></thead>"
+                jsonData.map(albaran => {
+                    tablaDeAlbaranes.innerHTML += 
+                        "<tr><td>" + albaran["numeroDeAlbaran"] 
+                        + "</td><td>" + albaran["cliente"]["nombre"]
+                        + "</td><td>" + albaran["destino"]["poblacion"]
+                        + "</td></tr>"
+                })
+            })
+            .catch((error) => alert("Se ha producido un error en la API de albaranes:\n" + error.message)); 
         alert(
-            "Todavia no se ha implementado la consulta de albaranes..." +
-                "\nLo siento, estoy trabajando en ello." +
-                "\n\n Cuando este implementada, se podran mostrar los últimos " +
-                cuantos +
-                " albaranes."
+        "Todavia no se ha implementado la consulta de albaranes..." +
+            "\nLo siento, estoy trabajando en ello." +
+            "\nPor ahora solo se muestran resultados parciales..." +
+            "\n\n Cuando este terminado el trabajo, se mostrarán todos los datos de los últimos " + cuantos + " albaranes."
         );
-    }
+    };
 });
