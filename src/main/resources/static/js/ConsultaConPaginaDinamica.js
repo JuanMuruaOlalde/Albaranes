c36a75caf6b57a6939a6dc0967f6697baa4fd258
Juan Murua Olalde <juan@susosise.es>
2023-01-31 09:57:12 +0100

    completar el javascript

--- src/main/resources/static/js/ConsultaConPaginaDinamica.js
+++ src/main/resources/static/js/ConsultaConPaginaDinamica.js
@@ -4,35 +4,54 @@ window.addEventListener("load", () => {
         event.preventDefault();
         obtenerAlbaranes();
     });
-
-    function obtenerAlbaranes() {
-        const datos = new FormData(frmObtenerAlbaranes);
-        const cuantos = Number(datos.get("cuantosAlbaranes"));
-        fetch("/albaranes/ultimos/" + cuantos)
-            .then((response) => {
-                if (!response.ok) {
-                    alert("No ha sido posible obtener datos de albaranes:\n" + response.status + " " + response.statusText);
-                }
-                return response.json();
-            })
-            .then((jsonData) => {
-                const tablaDeAlbaranes = document.getElementById("tablaDeLosAlbaranes");
-                tablaDeAlbaranes.innerHTML = 
-                    "<thead><th>numero</th><th>cliente</th><th>para entregar en</th></thead>"
-                jsonData.map(albaran => {
-                    tablaDeAlbaranes.innerHTML += 
-                        "<tr><td>" + albaran["numeroDeAlbaran"] 
-                        + "</td><td>" + albaran["cliente"]["nombre"]
-                        + "</td><td>" + albaran["destino"]["poblacion"]
-                        + "</td></tr>"
-                })
-            })
-            .catch((error) => alert("Se ha producido un error en la API de albaranes:\n" + error.message)); 
-        alert(
-        "Todavia no se ha implementado la consulta de albaranes..." +
-            "\nLo siento, estoy trabajando en ello." +
-            "\nPor ahora solo se muestran resultados parciales..." +
-            "\n\n Cuando este terminado el trabajo, se mostrarán todos los datos de los últimos " + cuantos + " albaranes."
-        );
-    };
 });
+
+function obtenerAlbaranes() {
+    const datos = new FormData(frmObtenerAlbaranes);
+    const cuantos = Number(datos.get("cuantosAlbaranes"));
+    fetch("/albaranes/ultimos/" + cuantos)
+        .then((response) => {
+            if (!response.ok) {
+                alert("No ha sido posible obtener datos de albaranes:\n" + response.status + " " + response.statusText);
+            }
+            return response.json();
+        })
+        .then((jsonData) => {
+            const tablaDeAlbaranes = document.getElementById("tablaDeLosAlbaranes");
+            tablaDeAlbaranes.innerHTML = 
+                '<thead><th>numero</th><th>cliente</th><th>para entregar en</th></thead>';
+            jsonData.map( albaran => {
+                const numeroDeAlbaranSeleccionado = albaran["numeroDeAlbaran"];
+                const lineas = albaran["lineas"];
+                tablaDeAlbaranes.innerHTML += 
+                    '<tr><td>' + numeroDeAlbaranSeleccionado 
+                    + '</td><td>' + albaran["cliente"]["nombre"]
+                    + '</td><td>' + albaran["destino"]["poblacion"]
+                    + '<td>'
+                    + '<button type="button"'
+                    + 'onclick="mostrarDetallesDelAlbaran(\'' + numeroDeAlbaranSeleccionado + '\'' 
+                    + ', \'' + JSON.stringify(lineas).replace(/\"/g, "&quot;") + '\')"'
+                    + '>Mostrar detalles</button>'
+                    + '</td></tr>';
+            });
+        })
+        .catch((error) => alert("Se ha producido un error en la API de albaranes:\n" + error.message)); 
+};
+
+
+function mostrarDetallesDelAlbaran(numeroDeAlbaran, lineasJSON) {
+    const tituloDelDetalleDeLineas = document.getElementById("tituloDelDetalleDeLineas");
+    tituloDelDetalleDeLineas.innerHTML = 
+        'Albarán ' + numeroDeAlbaran
+    const tablaDelDetalleDeLineas = document.getElementById("tablaDelDetalleDeLineas");
+    tablaDelDetalleDeLineas.innerHTML = 
+        '<thead><th>codigo de artículo</th><th>artículo</th><th>cantidad</th></thead>';
+    const lineas = JSON.parse(lineasJSON);
+    lineas.map( linea => {
+        tablaDelDetalleDeLineas.innerHTML += 
+            '<tr><td>' + linea["articulo"]["codigoDeArticulo"] 
+            + '</td><td>' + linea["articulo"]["nombreCorto"]
+            + '</td><td>' + linea["cantidad"]
+            + '</td></tr>';
+    });
+};
