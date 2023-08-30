ec95dafd50d70b34fc02abd721faaa8da411ec52
Juan Murua Olalde <juan@susosise.es>
2023-08-30 15:26:22 +0200

    corregir la forma de mostar los detalles
    Revisando el código me he dado cuenta que estaba matando moscas a cañonazos: enviando todos los detalles desde el servidor a través de la plantilla Thymeleaf, para luego ocultar los detalles que no deseaba mostrar usando un script.
    
    Ahora la parte que muestra los detalles se genera de forma dinámica, usando un script, a medida que se necesita.

--- src/main/resources/static/js/ConsultarAlbaranes.js
+++ src/main/resources/static/js/ConsultarAlbaranes.js
@@ -1,10 +1,26 @@
-function mostrarDetallesDelAlbaran(numeroDeAlbaran) {
-    const titulo = document.querySelector("#tituloDeLasLineasDeAlbaran");
-    titulo.innerHTML = "Albarán " + numeroDeAlbaran;
-    const tablas = document.querySelectorAll(".tablaAOcultar");
-    tablas.forEach((tabla) => {
-        tabla.style.display = "none";
-    });
-    const tablaAMostrar = document.querySelector("#" + numeroDeAlbaran);
-    tablaAMostrar.style.display = "block";
+function mostrarDetallesDelAlbaran(albaran) {
+    let detalles = `
+        <p>Albarán ${albaran.numeroDeAlbaran}</p>
+        <table>
+            <thead>
+                <th>codigo de artículo</th>
+                <th>artículo</th>
+                <th>cantidad</th>
+            </thead>
+            <tbody>
+        `;
+    for(linea of albaran.lineas) {
+        detalles += `
+            <tr>
+                <td>${linea.articulo.codigoDeArticulo}</td>
+                <td>${linea.articulo.nombreCorto}</td>
+                <td>${linea.cantidad}</td>
+            </tr>
+            `;
+    }
+    detalles += `
+            </tbody>
+        </table>
+        `;
+    document.querySelector("#paraMostrarDetallesDelAlbaran").innerHTML = detalles;
 }
